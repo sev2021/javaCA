6818d04fa0b028a23e33143c2e63601f2ec2aa26
sev2021 <seweryn.sapinski@gmail.com>
2022-01-30 01:51:37 +0000

    static variable opertations added

--- 01_29_Static/src/ATM.java
+++ 01_29_Static/src/ATM.java
@@ -1,6 +1,6 @@
 public class ATM{
 
-  // Step 2: Create your static variables here
+  // Static variables
   public static int totalMoney = 0;
   public static int numATMs = 0;
 
@@ -9,23 +9,27 @@ public class ATM{
 
   public ATM(int inputMoney){
     this.money = inputMoney;
+    numATMs += 1;
+    totalMoney += inputMoney;
   }
 
   public void withdrawMoney(int amountToWithdraw){
     if(amountToWithdraw <= this.money){
       this.money -= amountToWithdraw;
+      totalMoney -= amountToWithdraw;
     }
   }
 
   public static void main(String[] args){
-    // Step 1: Create your two ATMs here
+	System.out.println("Total number of ATMs: " + ATM.numATMs);
     ATM firstATM = new ATM(1000);
     ATM secondATM = new ATM(500);
+    System.out.println("Total number of ATMs: " + ATM.numATMs);
 
-    // Step 3: Print your static variable in three different ways here
-    System.out.println("ATM.totalMoney:\t\t" + ATM.totalMoney);
-    System.out.println("firstATM.totalMoney:\t" + firstATM.totalMoney);
-    System.out.println("secondATM.totalMoney:\t" + secondATM.totalMoney);
+    System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);  
+    firstATM.withdrawMoney(500);
+    secondATM.withdrawMoney(200);
+    System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);
 
   }
 
