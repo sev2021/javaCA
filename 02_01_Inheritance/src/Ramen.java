144965e5576308fe79a413611ab30a452936be82
sev2021 <seweryn.sapinski@gmail.com>
2022-02-01 02:39:38 +0000

    initial commit

---
+++ 02_01_Inheritance/src/Ramen.java
@@ -0,0 +1,15 @@
+public class Ramen extends Noodle {
+  
+  Ramen() {
+    
+    super(30.0, 0.3, "flat", "wheat flour");
+    
+  }
+  
+  // public boolean isTasty() {
+    
+  //   return false;
+    
+  // }
+  
+}
