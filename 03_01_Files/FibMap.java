3d9f78df1bb02a0c8d6bdc49020fd1b812badf4e
Sev <90162122+sev2021@users.noreply.github.com>
2022-03-03 02:21:55 +0000

    Fibonacci Sequence using recursion and memoization

--- 03_01_Files/FibMap.java
+++ 03_01_Files/FibMap.java
@@ -14,21 +14,36 @@
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
-public class FibMap {
+public class FibMap { //Fibonacci Sequence using recursion and memoization
 	//declare variable
 	
+	// static method FibMap is static because of recursion requirements
 	public static int FibMap(int n, Map<Integer, Integer> myMap) {
-		return n;
+		if(n < 2) {
+			return 1;
+		}
+		if(myMap.containsKey(n)) { //memoization - use of previously computed values
+			return myMap.get(n);
+		}
+		
+		int forMap = FibMap(n-1, myMap) + FibMap(n-2, myMap);
+		myMap.put(n, forMap);
+		// System.out.println(myMap.toString()); // debug only
+		return forMap;
 	}
 
 	public static void main(String[] args) {
 		//declare variable
+		int userInput;
 		Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
 		
-		System.out.println(FibMap(10, myMap));
-
-		myMap = new HashMap<Integer, Integer>();
-		System.out.println(FibMap(20, myMap));
+		//input
+		System.out.print("Enter number to compute Fibonacci Sequence: ");
+		userInput = new Scanner(System.in).nextInt();
+		
+		//compute
+		System.out.println(FibMap(userInput, myMap));
 	}
 }
